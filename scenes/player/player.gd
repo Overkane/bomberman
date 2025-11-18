c7cfc17e942d10bb25e7dfd516a8dfce8e00ff34
Overkane <overkane@mail.ru>
2025-11-19 04:54:22 +0500

    Add snapping to bomb placement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -13,7 +13,17 @@ func _physics_process(delta: float) -> void:
 func _input(event) -> void:
 	if event.is_action_pressed(&"place_bomb"):
 		var bomb: Bomb = BOMB_SCENE.instantiate()
-		bomb.global_position = global_position
+		# Bomb shoulbe be placed only in the center of each tile, which will be the half of tile size.
+		# To exclude position between tiles, need to check if one of coords can be divided by tile size.
+		# If it is, then need to add offset to the closest tile center.
+		var snapped_position: Vector2 = snapped(global_position, Vector2(Globals.TILE_SIZE, Globals.TILE_SIZE) / 2)
+		if is_zero_approx(fmod(snapped_position.x, Globals.TILE_SIZE)):
+			var offset_mult = 1 if global_position.x > snapped_position.x else -1
+			snapped_position.x += offset_mult * Globals.TILE_SIZE / 2.0
+		if is_zero_approx(fmod(snapped_position.y, Globals.TILE_SIZE)):
+			var offset_mult = 1 if global_position.y > snapped_position.y else -1
+			snapped_position.y += offset_mult * Globals.TILE_SIZE / 2.0
+		bomb.global_position = snapped_position
 		get_tree().root.add_child(bomb)
 
 
