4c0497dd98a25ef611f6dac9f8a10b090d39d2d4
Overkane <overkane@mail.ru>
2025-11-23 15:21:18 +0500

    Update bomb explosion sprite

--- scenes/bomb_explosion/bomb_explosion.gd
+++ scenes/bomb_explosion/bomb_explosion.gd
@@ -1,11 +1,11 @@
 class_name BombExplosion
 extends Area2D
 
-@onready var explosion_lifetime_timer: Timer = $ExplosionLifetimeTimer
+@onready var _explosion_lifetime_timer: Timer = $ExplosionLifetimeTimer
 
 
 func _ready() -> void:
-	explosion_lifetime_timer.timeout.connect(_on_explosion_lifetime_timeout)
+	_explosion_lifetime_timer.timeout.connect(_on_explosion_lifetime_timeout)
 	body_entered.connect(_on_body_entered)
 
 
