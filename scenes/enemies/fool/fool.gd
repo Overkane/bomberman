9dac956068b93241a1c4173ff3545e04a32f230a
Overkane <overkane@mail.ru>
2025-11-23 23:52:35 +0500

    Add dissolve effect on death

--- scenes/enemies/fool/fool.gd
+++ scenes/enemies/fool/fool.gd
@@ -1,35 +1,50 @@
 ## Simple enemy, which goes forward until it hits a wall/bomb/box,
-## then turns, preffering turning right and opposite direction being the least priority.
+## then turns, preffering turning right and opposite _direction being the least priority.
 class_name Fool
 extends CharacterBody2D
 
 signal exploded
 
-const BASE_SPEED := 100.0
+const _BASE_SPEED := 100.0
 # Since enemy collision shape a bit smaller, than distance between tiles, add additional
 # margin, so enemy doesn't rotate into the wall right next to it to move for a bit.
-const COLLISION_MARGIN := 1.0
+const _COLLISION_MARGIN := 1.0
 
-var direction := Vector2.RIGHT
+var _direction := Vector2.RIGHT
+var _is_dead := false
 
 
 func _physics_process(delta: float) -> void:
-	velocity = direction * BASE_SPEED
+	if _is_dead:
+		return
+
+	velocity = _direction * _BASE_SPEED
 	var collision_info: KinematicCollision2D = move_and_collide(velocity * delta)
 	if collision_info:
-		velocity = direction.rotated(PI / 2) * BASE_SPEED
-		collision_info = move_and_collide(velocity * delta, true, COLLISION_MARGIN)
+		velocity = _direction.rotated(PI / 2) * _BASE_SPEED
+		collision_info = move_and_collide(velocity * delta, true, _COLLISION_MARGIN)
 		if not collision_info:
-			direction = direction.rotated(PI / 2)
+			_direction = _direction.rotated(PI / 2)
 		else:
-			velocity = direction.rotated(-PI / 2 * BASE_SPEED)
-			collision_info = move_and_collide(velocity * delta, true, COLLISION_MARGIN)
+			velocity = _direction.rotated(-PI / 2 * _BASE_SPEED)
+			collision_info = move_and_collide(velocity * delta, true, _COLLISION_MARGIN)
 			if not collision_info:
-				direction = direction.rotated(-PI / 2)
+				_direction = _direction.rotated(-PI / 2)
 			else:
-				direction = direction.rotated(PI)
+				_direction = _direction.rotated(PI)
 
 
 func explode() -> void:
+	if _is_dead:
+		return
+
+	_is_dead = true
 	exploded.emit()
+	var tween = create_tween()
+	tween.tween_method(_set_percent, 1.0, 0.0, 0.85)
+	await tween.finished
 	queue_free()
+
+
+func _set_percent(percentage: float) -> void:
+	material.set_shader_parameter('percentage', percentage)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 signal exploded
 
 const _BASE_SPEED := 150.0
-const _BASE_MAX_LIVES := 3
+const _BASE_MAX_LIVES := 1
 const _BASE_MAX_BOMBS := 1
 const _BASE_BOMB_POWER := 1
 const _BOMB_SCENE := preload("uid://bpo5y5pvhbibe")
@@ -22,6 +22,7 @@ var _amount_of_lives: int:
 	get():
 		return _BASE_MAX_LIVES + BonusHandler.get_bonus(self, BonusHandler.BonusType.ADDITIONAL_LIFE) - _lives_wasted
 var _is_invulnerable := false
+var _is_dead := false
 
 @onready var _enemy_hurtbox: Area2D = %EnemyHurtbox
 @onready var _invulnerability_timer: Timer = %InvulnerabilityTimer
@@ -40,12 +41,18 @@ func _ready() -> void:
 	)
 
 func _physics_process(_delta: float) -> void:
+	if _is_dead:
+		return
+
 	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * _BASE_SPEED
 
 	move_and_slide()
 
 func _input(event) -> void:
+	if _is_dead:
+		return
+
 	if event.is_action_pressed(&"place_bomb") \
 		and _bomb_placement_checker.get_overlapping_bodies().size() == 0 \
 		and _current_bombs < _max_bombs:
@@ -69,15 +76,23 @@ func _input(event) -> void:
 
 
 func explode() -> void:
-	if _is_invulnerable:
+	if _is_invulnerable or _is_dead:
 		return
 
 	if _amount_of_lives > 0:
 		_lives_wasted += 1
 		if _amount_of_lives == 0:
+			_is_dead = true
+			var tween = create_tween()
+			tween.tween_method(_set_percent, 1.0, 0.0, 0.85)
+			await tween.finished
+			queue_free()
 			exploded.emit()
 			BonusHandler.clear_all_bonuses()
-			queue_free()
 		else:
 			_is_invulnerable = true
 			_invulnerability_timer.start(_INVULNERABILITY_TIME)
+
+
+func _set_percent(percentage: float) -> void:
+	material.set_shader_parameter('percentage', percentage)
