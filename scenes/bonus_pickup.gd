d18aa432bc262684fb48723a6db22591b3ce8d2b
Overkane <overkane@mail.ru>
2025-11-23 15:25:28 +0500

    Update bonus pickups sprite, fix UI issues

--- scenes/bonus_pickup.gd
+++ scenes/bonus_pickup.gd
@@ -4,9 +4,12 @@ extends Area2D
 var icon: Texture2D
 var bonus_type: BonusHandler.BonusType
 
+@onready var sprite_2d: Sprite2D = %Sprite2D
+
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	sprite_2d.texture = icon
 
 
 func _on_body_entered(body: Node):
