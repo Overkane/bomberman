4280fb9f9018c050519c874cc8d84ff47c6cb316
Overkane <overkane@mail.ru>
2025-11-23 15:22:32 +0500

    Update door sprite

--- scenes/level_door/level_door.gd
+++ scenes/level_door/level_door.gd
@@ -3,7 +3,11 @@ extends Area2D
 
 signal level_door_entered
 
+const OPEN_DOOR_SPRITE := preload("uid://bpc5b6ow8bgrp")
+
 var _is_open := false
+
+@onready var sprite_2d: Sprite2D = %Sprite2D
 
 
 func _ready() -> void:
@@ -12,6 +16,8 @@ func _ready() -> void:
 
 func open_door():
 	_is_open = true
+	sprite_2d.texture = OPEN_DOOR_SPRITE
+
 
 
 func _on_body_entered(_body: Node) -> void:
